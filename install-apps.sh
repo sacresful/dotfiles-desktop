35b7227c5cb93aa9755a500f83432b0a6c9a3124
sacresful <57761430+sacresful@users.noreply.github.com>
2022-10-31 19:47:08 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 apt_install () {
-	sudo apt-get install neofetch vifm rxvt-unicode sxiv firefox-esr obs-studio zip unzip alsa-utils pulseaudio mpv htop python3-pip libpangocairo-1.0-0 ffmpeg x264 x265 fonts-noto xinput cmus
+	sudo apt-get install \
+		neofetch \
+		vifm \
+		rxvt-unicode \
+		sxiv \
+		firefox-esr \
+		obs-studio \
+		zip unzip \
+		alsa-utils pulseaudio \
+		mpv \
+		htop \
+		python3-pip \
+		libpangocairo-1.0-0 \
+		ffmpeg x264 x265 
+		fonts-noto \
+		xinput \
+		cmus \
+		ncal \
 	sudo apt-get update 
 	sudo dpkg --add-architecture i386
 	sudo apt-get update

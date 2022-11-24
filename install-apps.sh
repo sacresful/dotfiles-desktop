893be011264770c922002b64e9c2704e11379447
sacresful <57761430+sacresful@users.noreply.github.com>
2022-11-24 15:38:19 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -21,8 +21,13 @@ apt_install () {
 		cmus \
 		ncal \
 		network-manager \
+		steam \
 	sudo apt-get update 
 	sudo dpkg --add-architecture i386
+	sudo apt-get update
+	sudo apt install libgl1-mesa-dri:i386 libgl1:i386
+	sudo apt-get upgrade steam -f
+	sudo apt install nvidia-driver-libs:i386
 	sudo apt-get update
 }
 apt_install

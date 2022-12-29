a264689c053f0193ef6e08df6da30729234fc80e
sacresful <57761430+sacresful@users.noreply.github.com>
2022-12-29 12:54:38 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt_install () {
+	sudo dpkg --add-architecture i386
+	sudo apt-get update
 	sudo apt-get install \
 		#basic
 		neofetch \
@@ -29,11 +31,9 @@ apt_install () {
 		cmus \
 		ncal \
 		steam \
+		gnome-keyring \
 	sudo apt-get update 
-	sudo dpkg --add-architecture i386
-	sudo apt-get update
 	sudo apt install libgl1-mesa-dri:i386 libgl1:i386
-	sudo apt-get upgrade steam -f
 	sudo apt install nvidia-driver-libs:i386
 	sudo apt-get update
 	sudo apt install gnupg2 software-properties-common apt-transport-https curl
